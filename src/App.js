f6bbd73f643ad1d9432c5490c19f9866c2a31fe1
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2022-09-23 22:51:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
 
 function App() {
+  const [place, setPlace] = useState(0);
+  const {id, image, titel, description, showMore} = data[place];
+
+  //Read more button
+  const [readMore, setReadMore] = useState(false);
+  const readMoreClick = (element) => {
+    element.showMore = !element.showMore;
+    setReadMore(!readMore);
+  }
+
+  //Slides
+  const back = () => {
+    setPlace (place => {
+      place --;
+      if (place < 0) {
+        place = data.length - 1;
+      }
+      return place;
+    })
+  }
+
+  const next = () => {
+    setPlace (place => {
+      place ++;
+      if (place > data.length -1) {
+        place = 0;
+      }
+      return place;
+    })
+  }
+
+  // to Do List
+  const [toDoList, setToDoList] = useState([]); 
+  const onClick = () => {
+    setToDoList (item => [...item, 'Visit ' + titel]);
+    console.log(toDoList);
+  }
+
+  //Deleting an item from to Do List
+  const [visit, setVisit] = useState(toDoList)
+  const removeElement = (id) => {
+    let newList = visit.filter(item => item.id !== id);
+    setVisit(newList);
+  }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h1>Top 10 Things to Do in Paris</h1>
+      <div>
+        <button onClick={back}>back</button>
+        <img src={image} alt='paris' width="700px"></img>
+        <button onClick={next}>next</button>
+      </div>
+      <h2>{id} - {titel}</h2>
+      <div>
+        <p>{showMore ? description : description.substring(0, 220) + '...'}
+          <button onClick={() => readMoreClick(data[place])}>{showMore ? 'Read less' : 'Read more'}</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div>
+        <button onClick={onClick}>Add to my to Do List</button>
+      </div>
+      {toDoList.map((element, index) => {
+        const {id} = element;
+
+        return (
+          <div key={index}>
+            <ul>
+              <li>{element} <button onClick={() => removeElement(id)}>Delete</button></li>
+            </ul>
+          </div>
+        )
+      })}
+      <div>
+        <button onClick={() => setToDoList([])}>Delete All</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,92 @@
+import imgOne from './img/1.jpg';
+import imgTwo from './img/2.jpg';
+import imgThree from './img/3.jpg';
+import imgFour from './img/4.jpg';
+import imgFive from './img/5.jpg';
+import imgSix from './img/6.jpg';
+import imgSeven from './img/7.jpg';
+import imgEight from './img/8.jpg';
+import imgNine from './img/9.jpg';
+import imgTen from './img/10.jpg';
+
+export const data = [
+    {
+        id: 1,
+        image: imgOne,
+        titel: 'Eiffel Tower',
+        description: 'You can reach Paris most famous landmark from the Bir-Hakeim, Trocadéro or Ecole Militaire métro stops, serviced by lines 6, 8 and 9. The Eiffel Tower, located on the western side of the city, is open every day of the year, from 9 a.m. to 12:30 p.m. from mid-June to early September, and from 9:30 a.m. to 11: 45 p.m. the rest of the year. Admission prices vary depending on how high you wish to go and how you choose to get there (elevator or stairs).',
+        showMore: false
+    },
+
+    {
+        id: 2,
+        image: imgTwo,
+        titel: 'Arc de Triomphe',
+        description: '',
+        showMore: false
+    }, 
+
+    {
+        id: 3,
+        image: imgThree,
+        titel: 'Palais Royale',
+        description: '',
+        showMore: false
+    },
+
+    {
+        id: 4,
+        image: imgFour,
+        titel: 'Sacre Coeur Basilica',
+        description: `Rising high above Paris, the Sacré-Coeur (meaning "Sacred Heart") looks more like a white castle than a basilica – but that's what it is. Towering over the eclectic neighborhood of Montmartre (once a hangout for Paris' bohemian crowd), this Roman-Byzantine masterpiece is easily recognized by its ornate ivory domes. As blanched as it may appear on the outside, the basilica's interior is a sight worth beholding: The ceilings glitter with France's largest mosaic, which depicts Jesus rising alongside the Virgin Mary and Joan of Arc. `,
+        showMore: false
+    },
+
+    {
+        id: 5,
+        image: imgFive,
+        titel: 'Monmartre',
+        description: '',
+        showMore: false
+    },
+
+    {
+        id: 6,
+        image: imgSix,
+        titel: 'Seine',
+        description: '',
+        showMore: false
+    },
+
+    {
+        id: 7,
+        image: imgSeven,
+        titel: 'Notre-Dame de Paris',
+        description: '',
+        showMore: false
+    },
+
+    {
+        id: 8,
+        image: imgEight,
+        titel: 'Lafayette Gourmet',
+        description: '',
+        showMore: false
+    },
+
+    {
+        id: 9,
+        image: imgNine,
+        titel: 'Moulin Rouge',
+        description: '',
+        showMore: false
+    },
+
+    {
+        id: 10,
+        image: imgTen,
+        titel: 'Louvre',
+        description: '',
+        showMore: false
+    }
+]
